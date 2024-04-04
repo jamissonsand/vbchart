fc5a072010f16135c399ad9b867ba6526e0fa4c3
unknown <jamissons.and@gmail.com>
2024-04-04 12:54:10 -0300

    grid

--- vchart.js
+++ vchart.js
@@ -17,90 +17,102 @@ $(document).ready(function () {
     var points = [{ x: pointX, y: pointY }];
 
     var my_point = new Image();
-    my_point.src = "./round-point.png";
+    my_point.src = "./circle.svg";
 
     var img = new Image();
     img.src = "./grafico2.svg";
 
-    // Função para redimensionar o gráfico
-    function resizeChart() {
-        var container = document.getElementById("chart-container");
-        var width = container.offsetWidth;
-        var height = (width / 600) * 350; // Mantendo a proporção 600 por 350
+    var legendImg = new Image();
+    legendImg.src = "./legenda.svg";
 
-        ctx.canvas.width = width;
-        ctx.canvas.height = height;
+    img.onload = function() { // Aguarde até que a imagem seja carregada antes de desenhar o gráfico
+        // Função para redimensionar o gráfico
+        function resizeChart() {
+            var container = document.getElementById("chart-container");
+            var width = container.offsetWidth;
+            var height = (width / 960) * 480; // Mantendo a proporção 600 por 350
 
-        myChart.resize(width, height);
-    }
+            ctx.canvas.width = width;
+            ctx.canvas.height = height;
 
-    // Redimensionar o gráfico quando a janela for redimensionada
-    window.addEventListener('resize', resizeChart);
+            myChart.resize(width, height);
+        }
 
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var myChart = new Chart(ctx, {
-        type: "scatter",
-        data: {
-            datasets: [{
-                label: "",
-                data: points,
-                backgroundColor: "transparent",
-                borderColor: "black",
-                borderWidth: 1,
-                pointRadius: 10,
-                pointStyle: my_point,
-                pointBorderColor: "black",
-                pointBackgroundColor: "black",
-                pointBorderWidth: 1
-            }]
-        },
-        options: {
-            plugins: {
-                legend: { display: false },
-                tooltip: { enabled: false }
+        // Redimensionar o gráfico quando a janela for redimensionada
+        window.addEventListener('resize', resizeChart);
+
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myChart = new Chart(ctx, {
+            type: "scatter",
+            data: {
+                datasets: [{
+                    label: "",
+                    data: points,
+                    backgroundColor: "transparent",
+                    borderColor: "black",
+                    borderWidth: 1,
+                    pointRadius: 10,
+                    pointStyle: my_point,
+                    pointBorderColor: "black",
+                    pointBackgroundColor: "black",
+                    pointBorderWidth: 1
+                }]
             },
-            scales: {
-                y: {
-                    display: false,
-                    min: 0,
-                    max: 12,
-                    ticks: {
+            options: {
+                plugins: {
+                    legend: { display: false },
+                    tooltip: { enabled: false }
+                },
+                scales: {
+                    y: {
                         display: false,
-                        stepSize: 2 // Definindo o espaçamento entre as linhas do grid em 2 unidades de altura
+                        min: 0,
+                        max: 12,
+                        ticks: {
+                            display: false,
+                            stepSize: 2 // Definindo o espaçamento entre as linhas do grid em 2 unidades de altura
+                        }
+                    },
+                    x: {
+                        display: false,
+                        min: 1,
+                        max: 25,
+                        ticks: {
+                            display: false,
+                            stepSize: 1 // Definindo o espaçamento entre as linhas do grid em 1 unidade de largura
+                        }
                     }
                 },
-                x: {
-                    display: false,
-                    min: 1,
-                    max: 25,
-                    ticks: {
-                        display: false,
-                        stepSize: 1 // Definindo o espaçamento entre as linhas do grid em 1 unidade de largura
-                    }
-                }
+                interaction: { intersect: false }
             },
-            interaction: { intersect: false }
-        },
-        // Adicionando a imagem ao gráfico
-        plugins: [{
-            beforeDraw: function(chart) {
-                var ctx = chart.ctx;
-                var xAxis = chart.scales['x'];
-                var yAxis = chart.scales['y'];
-                
-                ctx.drawImage(img, xAxis.left, yAxis.top, xAxis.width, yAxis.height);
-            }
-        }]
-    });
+            // Adicionando a imagem ao gráfico
+            plugins: [{
+                beforeDraw: function(chart) {
+                    var ctx = chart.ctx;
+                    var xAxis = chart.scales['x'];
+                    var yAxis = chart.scales['y'];
+                    
+                    // Desenhar imagem do gráfico 10px menor em cada lado
+                    var graphWidth = xAxis.width * 0.9; // 80% da largura do contêiner
+var graphHeight = yAxis.height * 0.87; // 80% da altura do contêiner
+var graphX = xAxis.left + (xAxis.width * 0.05); // 10% de margem esquerda
+var graphY = yAxis.top + (yAxis.height * 0.07); // 10% de margem superior
+ctx.drawImage(img, graphX, graphY, graphWidth, graphHeight);
+// Desenhar legenda por baixo
+ctx.drawImage(legendImg, xAxis.left, yAxis.top, xAxis.width, yAxis.height);
+                }
+            }]
+        });
 
-    // Redimensionar o gráfico ao carregar a página
-    resizeChart();
-
-    window.addEventListener("beforeprint", () => {
-        myChart.resize(600, 350);
-    });
-
-    window.addEventListener("afterprint", () => {
+        // Redimensionar o gráfico ao carregar a página
         resizeChart();
-    });
+
+        window.addEventListener("beforeprint", () => {
+            myChart.resize(960, 480);
+        });
+
+        window.addEventListener("afterprint", () => {
+            resizeChart();
+        });
+    };
 });
